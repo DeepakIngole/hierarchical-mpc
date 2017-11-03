d319cb28ec481977b39c948f0d9c9a5a449531ee
trinhvv <vanvuong.trinh@gmail.com>
2017-11-03 14:49:29 +0100

    Commit time: 2017-11-03 14:49:29

--- benchmark/phi_criterion.m
+++ benchmark/phi_criterion.m
@@ -1,36 +1,33 @@
 function r=phi_criterion(x,scenario)
-  Tsim=evalin('base','Tsim;');
-  Ts=evalin('base','Ts;');
-  % number of states of subsystems
-  r=0;
-  M=size(x,1)/4; %4 states for each subsystem
-  P=getP();
-  if scenario~=3
+Tsim=evalin('base','Tsim;');
+Ts=evalin('base','Ts;');
+r=0;
+M=size(x,1)/4;
+P=getP();
+if scenario~=3
     P=P(:,1:M);
-  else
+else
     P=P(:,[1 2 3 5]);
-  end
-  for k=1:Tsim
+end
+for k=1:Tsim
     for i=1:size(P,1)
       deltaTeta_i=x(1+(i-1)*4,k);
-      [tmp js]=find(P(i,:));
+      [~,js]=find(P(i,:));
       for j=js
         deltaTeta_j=x(1+(j-1)*4,k);
         DeltaPtie=P(i,j)*(deltaTeta_i-deltaTeta_j);
         r = r + abs(DeltaPtie)*Ts;
       end
     end
-  end
-  r=r/Tsim;
 end
+r=r/Tsim;
 
 function P=getP()
-  str=evalin('base','who(''P*'')');
-  for i=1:length(str)
-    if ~isempty(str2num(str{i}(2:end)))
-      j=str2num(str{i}(2));
-      k=str2num(str{i}(3));
+str=evalin('base','who(''deltaPtie*'')');
+for i=1:length(str)
+    if ~isempty(str2num(str{i}(10:end)))
+      j=str2num(str{i}(10));
+      k=str2num(str{i}(11));
       P(j,k)=evalin('base',str{i});
     end
-  end
 end
--- script_valid.m
+++ script_valid.m
@@ -1,28 +1,45 @@
 clear mex;clear all;close all;clc;
 addpath(genpath(pwd));
 load agent;
+load valid_scenario;
 
 HDMPC.Ns=4;
 HDMPC.aaParam=struct('topology',...
                      {[],[1],[],[];[1],[],[1],[];[],[1],[],[1];[],[],[1],[]},...
                      'm',5,'droptol',1e-1,'beta',1,'AAstart',0,...
-                     'smax',3e1,'atol',1e-4,'rtol',1e-4,'verbose',1);
+                     'smax',1e2,'atol',1e-5,'rtol',1e-5,'verbose',0);
 HDMPC.nlpParam=struct('algorithm',NLOPT_LN_BOBYQA,...
                      'pi',[1;1;1;1],'rmin',[-1;-1;-1;-1],'rmax',[+1;+1;+1;+1],...
-                     'kmax',1e2,'axtol',1e-4*ones(4,1),'aftol',1e-4,...
+                     'kmax',1e2,'axtol',1e-5*ones(4,1),'aftol',1e-5,...
                      'initstep',1*ones(4,1),'verbose',0);
 
-for i=1:HDMPC.Ns
-    SystemData(i).x=-1e-1+2e-1*rand(SubsystemMPC(i).model.nx,1);
-    SystemData(i).w=zeros(SubsystemMPC(i).model.nw,1);
-    AgentData(i).vpred_sequences=...
-        zeros(length(SubsystemMPC(i).param.vIndex),...
-        SubsystemMPC(i).model.nv);
-    AgentData(i).ustar_sequences=[];
-    AgentData(i).xpred_sequences=[];
-    AgentData(i).ypred_sequences=[];
-    AgentData(i).zpred_sequences=[];
-    AgentData(i).J=[];
-    AgentData(i).yd=0;
+%% EVALUATION
+% SIMULATION
+mode=1;
+if mode<0
+    set_param('hdmpc_pns/HDMPC','commented','on');
+    set_param('hdmpc_pns/From Workspace2','commented','off');
+else
+    set_param('hdmpc_pns/HDMPC','commented','off');
+    set_param('hdmpc_pns/From Workspace2','commented','on');
 end
-[ropt,Jopt,nJ,AgentData]=master_optimize(HDMPC,SubsystemMPC,SystemData,AgentData);
+sim('hdmpc_pns');
+
+t=x.time;
+x=x.signals.values;
+u=u.signals.values;
+deltaPref=deltaPref.signals.values;
+deltaFrequency=deltaFrequency.signals.values;
+deltaPtie12=deltaPtie12.signals.values;
+deltaPtie23=deltaPtie23.signals.values;
+deltaPtie34=deltaPtie34.signals.values;
+
+% ETA CRITERIA
+etaCrit=eta_criterion(x',xO',u',uO',Q,R);
+
+% STORAGE
+if mode<0
+    save pns_cmpc_valid t deltaPref deltaFrequency deltaPtie12 deltaPtie23 deltaPtie34;
+else
+    save pns_dmpc_valid t deltaPref deltaFrequency deltaPtie12 deltaPtie23 deltaPtie34;
+end
